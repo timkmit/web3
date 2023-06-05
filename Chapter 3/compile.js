611100034957e26e76f4deeefd6dc71aa1f8112d
Timur <timengalytchev@mail.ru>
2023-06-05 03:33:53 +0300

    need fix tests

--- Chapter 3/compile.js
+++ Chapter 3/compile.js
@@ -1,27 +1,33 @@
 const path = require('path');
-const greetingsPath = path.resolve(__dirname, 'contracts', 'Greetings.sol');
-
 const fs = require('fs');
 const solc = require('solc');
 
+const greetingsPath = path.resolve(__dirname, 'contracts', 'Greetings.sol');
 const source = fs.readFileSync(greetingsPath, 'utf8');
 
 const input = {
-  language: 'Solidity',
-  sources: {
-    'Greetings.sol': {
-      content: source,
+    language: 'Solidity',
+    sources: {
+        'Greetings.sol': {
+            content: source,
+        },
     },
-  },
-  settings: {
-    outputSelection: {
-      '*': {
-        '*': ['*'],
-      },
+    settings: {
+        outputSelection: {
+            '*': {
+                '*': ['abi', 'evm.bytecode'],
+            },
+        },
     },
-  },
 };
 
 const output = JSON.parse(solc.compile(JSON.stringify(input)));
+const contract = output.contracts['Greetings.sol'].Greetings;
 
-console.log(output);
+const interface = contract.abi;
+const bytecode = contract.evm.bytecode.object;
+
+module.exports = {
+    interface,
+    bytecode,
+};
